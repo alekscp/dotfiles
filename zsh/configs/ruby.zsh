cc1ef25efc91e3b608d9213d6a3de45953e6ecdf
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2019-02-13 14:22:18 +0100

    Set default global ruby to 2.5.0

--- zsh/configs/ruby.zsh
+++ zsh/configs/ruby.zsh
@@ -8,5 +8,7 @@ if [[ -e /usr/local/share/chruby ]]; then
   source /usr/local/share/chruby/auto.sh
   if [[ -f ~/.ruby-version ]]; then
     chruby $(cat ~/.ruby-version)
+  else
+    chruby ruby-2.5.0
   fi
 fi
