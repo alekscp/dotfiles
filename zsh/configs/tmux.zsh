77fc78e9decad11bd113a83496461a607f4dcf8c
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2019-12-06 16:33:47 +0100

    Only spin up tmux in iterm

--- zsh/configs/tmux.zsh
+++ zsh/configs/tmux.zsh
@@ -1,9 +1,14 @@
+_inside_iterm() {
+  [ "$TERM_PROGRAM" = "iTerm.app" ]
+}
+
 _not_inside_tmux() {
   [[ -z "$TMUX" ]]
 }
 
+# Spin in Tmux if we are in iTerm and not already inside a Tmux session
 ensure_tmux_is_running() {
-  if _not_inside_tmux; then
+  if _not_inside_tmux && _inside_iterm; then
     tat
   fi
 }
