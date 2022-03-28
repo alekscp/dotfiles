44dacaddf82234b426e7f5305f142d96cae6e088
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2022-03-28 13:18:35 +0200

    Stuff

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -239,10 +239,10 @@ local on_attach = function(client, bufnr)
   buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
   buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
   buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+  buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+  buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+  buf_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
   buf_set_keymap("n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 end
 
@@ -298,6 +298,7 @@ cmd "set noswapfile"
 cmd "set nobackup"
 cmd "set scrolloff=8"
 cmd "set termguicolors"
+cmd "set shell=/bin/bash" -- Performance boost
 cmd "au CursorHold,CursorHoldI,FocusGained,BufEnter * checktime"
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
