9fe4df64d25f541f2195bb145a99645deacc0091
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2022-06-30 14:22:03 +0200

    Custom conf for ARCx

--- zsh/configs/arcx.zsh
+++ zsh/configs/arcx.zsh
@@ -1,1 +1,4 @@
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/aclapinpepin/Downloads/gcp.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/aclapinpepin/.gcp/titan.json"
+# export GOOGLE_APPLICATION_CREDENTIALS="/Users/aclapinpepin/.gcp/sapphire.json"
+
+export SPARK_CONF_DIR=${HOME}/.spark/
---
+++ zsh/configs/conda.zsh
@@ -0,0 +1,18 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/aclapinpepin/mambaforge/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/aclapinpepin/mambaforge/etc/profile.d/conda.sh" ]; then
+        . "/Users/aclapinpepin/mambaforge/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/aclapinpepin/mambaforge/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+if [ -f "/Users/aclapinpepin/mambaforge/etc/profile.d/mamba.sh" ]; then
+    . "/Users/aclapinpepin/mambaforge/etc/profile.d/mamba.sh"
+fi
+# <<< conda initialize <<<
