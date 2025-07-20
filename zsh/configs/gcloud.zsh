eac686155ae3ce096576a202bc343793623e76d5
Aleks CP <me@alekscp.com>
2025-07-20 13:58:36 +0200

    chore: add configs for gcloud sdk cli

---
+++ zsh/configs/gcloud.zsh
@@ -0,0 +1,9 @@
+# Google Cloud SDK
+# Setup PATH and completions for gcloud CLI.
+if [ -f /Users/aclapinpepin/google-cloud-sdk/path.zsh.inc ]; then
+  source /Users/aclapinpepin/google-cloud-sdk/path.zsh.inc
+fi
+
+if [ -f /Users/aclapinpepin/google-cloud-sdk/completion.zsh.inc ]; then
+  source /Users/aclapinpepin/google-cloud-sdk/completion.zsh.inc
+fi
